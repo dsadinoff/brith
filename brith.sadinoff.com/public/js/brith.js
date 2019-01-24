0c6c8b3bd04bf40dd399551b693a64b6ebb306a1
Danny Sadinoff <danny@sadinoff.com>
2019-01-24 14:12:29 +0000

    visual cleanup

--- brith.sadinoff.com/public/js/brith.js
+++ brith.sadinoff.com/public/js/brith.js
@@ -1,6 +1,26 @@
  "use strict";
 
+var currentResponse = {};
+
+// Initialize the widget when the DOM is ready ';
+
+
+
+function processServerDisplayResponse(obj){
+    currentResponse = obj
+    console.log(currentResponse);
+    $('.display').show();
+    $('.hebrew-col').html(currentResponse.hebrew);
+    $('.braille-col').html(currentResponse.braille);
+}
+
+
 $(document).ready(function(){
+    function clearOutput(){
+	$('.hebrew-col').empty();
+	$('.braille-col').empty();
+	$('.display').hide();
+    }
     function setupMouseOverEvents(){
 	$('.braille-col').on('mouseenter','.br-word',
 			     function(e){
@@ -63,9 +83,64 @@ $(document).ready(function(){
 	
     }
 
+    function setupUpload(){
+	$('#activateFileUpload').click(function(){
+	    $('#uploader').show(300);
+	    $('#activateFileUpload').hide(200);
+	    clearOutput();
+	});
 
+	$('.download-brf').click(function(){
+	    window.location.href=currentResponse.path;
+	});
 
+	$("#uploader").plupload({
+	    // General settings
+	    runtimes : 'html5,html4',
+	    url : "/translate-file",
+	    
+	    // Maximum file size
+	    max_file_size : '2mb',
+	    
+	    chunk_size: '1mb',
+	    
+	    // Specify what files to browse for
+	    filters : [
+		
+		
+	    ],
+	    
+	    // Rename files by clicking on their titles
+	    rename: true,
+	    
+	    // Sort files
+	    sortable: true,
+	    
+	    // Enable ability to drag'n'drop files onto the widget (currently only HTML5 supports that)
+	    dragdrop: true,
+	    
+	    // Views to activate
+	    views: {
+		list: true,
+		thumbs: false, // Show thumbs
+		active: 'list'
+	    },
+	    multi_selection: false,
+	    multipart_params:{
+		fmt : 'displayMarkup'
+	    },
+	    init:{
+		FileUploaded: function(up,file,info){
+		    var resp = JSON.parse(info.response);
+		    processServerDisplayResponse(resp);
+		}	  
+	    }
+	    
+	});
+    }
+    
     setupMouseOverEvents();
     setupFetchSefaria();
+    setupUpload();
     
 });
